24f6534d72e155d1483ba585b0a5652f9fba4d3e
Alexsanchez-WP <alexsanchez.wp@gmail.com>
2021-12-09 20:48:54 -0500

    Se crea archivo de variables de entorno

--- backend/api.js
+++ backend/api.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+require('dotenv').config()
+const PORT = process.env.APP_PORT || 8080;
+
 
 app.get("/", function (req, res) {
     res.send("Api paqueteria grupo 6");
 });
 
-app.listen(3000);
+app.listen(PORT, () => console.log('Escuchando por le puerto: ', PORT));
