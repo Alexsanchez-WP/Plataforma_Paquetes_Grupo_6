d7b426b7c101b17afb6416d59c5eb59c2bc0391e
Leonardo Barona <lebab1990@gmail.com>
2021-12-12 07:28:50 -0500

    Archivos de configuracion, conexion y CRUD a la base de datos, creacion de la collecion en MongoDB

---
+++ backend/controllers/tareas/metodos.js
@@ -0,0 +1,56 @@
+import { ObjectId } from "mongodb";
+import { mongoDBClient } from "../../db/cliente";
+
+//Mostrar todas las tareas
+export const getAllTasks = async() => {
+    const cursor = await mongoDBClient.db('paquetes').collection('tareas').find();
+    const data = await cursor.toArray();
+    return await data;
+}
+
+//Mostrar todas las tareas por estados
+export const getAllTasksByStatus = async(estado) => {
+    const cursor = await mongoDBClient.db('paquetes').collection('tareas').find({ isCompleted: estado });
+    const data = await cursor.toArray();
+    return await data;
+}
+
+//Recuperar una tareas especifica
+export const getTaskById = async(id) => {
+    const queryId = new ObjectId(id);
+    const cursor = await mongoDBClient.db('paquetes').collection('tareas').findOne({ _id: queryId });
+    const data = await cursor.toArray();
+    return await data;
+}
+
+//Agregar un registro 
+export const createTask = async(newTask) => {
+    const { creator, title, task } = newTask;
+    return await mongoDBClient.db('paquetes').collection('tareas').insertOne({
+        isCompleted: false,
+        creator: creator,
+        title: title,
+        task: task,
+        createDate: new Date(),
+        finish: null
+    });
+}
+
+//Actualizar un registro
+export const updateTask = async(taskId, newTask) => {
+    const queryId = new Object(taskId);
+    const { creator, title, task } = newTask;
+    return await mongoDBClient.db('paquetes').collection('tareas').updateOne({ _id: queryId }, { $set: { creator, title, taks } });
+}
+
+//Eliminar un registro
+export const deleteTask = async(taskId) => {
+    const queryId = new Object(taskId);
+    return await mongoDBClient.db('paquetes').collection('tareas').deleteOne({ _id: queryId });
+}
+
+//Actualizar un registro - Finalaizar tarea
+export const completeTask = async(taskId) => {
+    const queryId = new Object(taskId);
+    return await mongoDBClient.db('paquetes').collection('tareas').updateOne({ _id: queryId }, { $set: { isCompleted: true, finish: new Date() } });
+}
